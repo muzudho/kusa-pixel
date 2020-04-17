ce7b6660488abd8981229e6b747ef1568a7a0248
むずでょ <muzudho1@gmail.com>
2020-04-17 21:07:16 +0900

    マウス座標

--- piston-pixel-art/src/res/window/window.rs
+++ piston-pixel-art/src/res/window/window.rs
@@ -13,19 +13,34 @@ pub fn show_window(png_path: &str) {
         .unwrap();
 
     let texture = create_texture(png_path, &mut window);
+    let mut cursor = [0.0, 0.0];
 
     // Event loop.
     window.set_lazy(true);
     while let Some(e) = window.next() {
+        // マウスカーソルの座標を補足するぜ☆（＾～＾）
+        e.mouse_cursor(|pos| {
+            cursor = pos;
+            // println!("Mouse moved '{} {}'", pos[0], pos[1]);
+        });
+
         // update
         if let Some(_button) = e.press_args() {
             println!("ボタンが押されたぜ☆（＾～＾）");
+            println!("Click ({}, {})", cursor[0], cursor[1]);
         }
 
         // draw
         window.draw_2d(&e, |c, g, _| {
             clear([1.0; 4], g);
             image(&texture, c.transform.zoom(2.0), g);
+
+            rectangle(
+                [1.0, 0.0, 0.0, 1.0], // red
+                [cursor[0], cursor[1], 100.0, 100.0],
+                c.transform,
+                g,
+            );
         });
     }
 }
